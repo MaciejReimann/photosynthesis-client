99d916ed5567b1dc3d73e942a2a5243de31876f7
Maciej Reimann <maciej.reimann@gmail.com>
2020-03-29 19:43:02 +0200

    pricetags added

--- src/player-repo/PlayerRepoBoard.tsx
+++ src/player-repo/PlayerRepoBoard.tsx
@@ -7,10 +7,22 @@ import styles from "./PlayerRepoBoard.module.scss"
 export function PlayerRepoBoard() {
   return (
     <div className={styles.wrapper}>
-      <TreePlaceholder />
-      <TreePlaceholder size={TreeSize.Medium} />
-      <TreePlaceholder size={TreeSize.Large} />
       Board
+      <div className={styles["placeholders-wrapper"]}>
+        <div className={styles["placeholders"]}>
+          <TreePlaceholder /> <TreePlaceholder /> <TreePlaceholder />
+          <TreePlaceholder />
+        </div>
+        <div className={styles["placeholders"]}>
+          <TreePlaceholder size={TreeSize.Medium} />
+          <TreePlaceholder size={TreeSize.Medium} />
+          <TreePlaceholder size={TreeSize.Medium} />
+        </div>
+        <div className={styles["placeholders"]}>
+          <TreePlaceholder size={TreeSize.Large} />
+          <TreePlaceholder size={TreeSize.Large} />
+        </div>
+      </div>
     </div>
   )
 }
