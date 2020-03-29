1da6306695a79215b78b32096d38138831665572
Maciej Reimann <maciej.reimann@gmail.com>
2020-03-29 13:51:00 +0200

    hexgrid imported and working

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import { HexGrid, Layout, Hexagon, Text, Path, Hex } from "react-hexgrid"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <HexGrid width={1200} height={800} viewBox="-50 -50 100 100">
+        {/* Grid with manually inserted hexagons */}
+        <Layout
+          size={{ x: 5, y: 5 }}
+          flat={true}
+          spacing={1.1}
+          origin={{ x: 0, y: 0 }}
         >
-          Learn React
-        </a>
-      </header>
+          <Hexagon q={0} r={0} s={0} />
+          {/* Using pattern (defined below) to fill the hexagon */}
+          <Hexagon q={0} r={-1} s={1} fill="pat-1" />
+          <Hexagon q={0} r={1} s={-1} />
+          <Hexagon q={1} r={-1} s={0}>
+            <Text>1, -1, 0</Text>
+          </Hexagon>
+        </Layout>
+      </HexGrid>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
