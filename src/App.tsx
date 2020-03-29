e598162a2d9581d3883f5d5211ed97e03a630389
Maciej Reimann <maciej.reimann@gmail.com>
2020-03-29 15:23:22 +0200

    hexagon grid created

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,31 @@
 import React from "react"
-import { HexGrid, Layout, Hexagon, Text, Path, Hex } from "react-hexgrid"
+import {
+  GridGenerator,
+  HexGrid,
+  Layout,
+  Hexagon,
+  Text,
+  Path,
+  Hex
+} from "react-hexgrid"
 
 function App() {
+  const grid = GridGenerator.hexagon(3)
   return (
     <div className="App">
       <HexGrid width={1200} height={800} viewBox="-50 -50 100 100">
         {/* Grid with manually inserted hexagons */}
         <Layout
           size={{ x: 5, y: 5 }}
-          flat={true}
+          flat={false}
           spacing={1.1}
           origin={{ x: 0, y: 0 }}
         >
-          <Hexagon q={0} r={0} s={0} />
-          {/* Using pattern (defined below) to fill the hexagon */}
-          <Hexagon q={0} r={-1} s={1} fill="pat-1" />
-          <Hexagon q={0} r={1} s={-1} />
-          <Hexagon q={1} r={-1} s={0}>
-            <Text>1, -1, 0</Text>
-          </Hexagon>
+          {grid.map((hex: any, i: number) => (
+            <Hexagon key={i} q={hex.q} r={hex.r} s={hex.s}>
+              <Text>1, -1, 0</Text>{" "}
+            </Hexagon>
+          ))}
         </Layout>
       </HexGrid>
     </div>
