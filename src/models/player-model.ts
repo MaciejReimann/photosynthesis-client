05554fd00bc081ba072d5a1a13d6882eee8d23bb
Maciej Reimann <maciej.reimann@gmail.com>
2020-04-12 19:03:25 +0200

    player model defined

--- src/models/player-model.ts
+++ src/models/player-model.ts
@@ -1,1 +1,30 @@
-export class Player {}
+// import { Repository } from "./repository-model"
+export class Player {
+  private name: string = ""
+  private isActive: boolean = false
+
+  constructor(readonly id: number) {
+    this.id = id
+    // this.repository = new Repository()
+  }
+
+  // setters
+
+  setName(name: string): void {
+    this.name = name
+  }
+
+  setIsActive(value: boolean): void {
+    this.isActive = value
+  }
+
+  // getters
+
+  getName(): string {
+    return this.name
+  }
+
+  getIsActive(): boolean {
+    return this.isActive
+  }
+}
